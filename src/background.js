b2d6a81994a052a8dd467cacdcfff62b29a1879f
David, Shin <dirmich@gmail.com>
2021-10-21 18:21:07 +0900

    a

---
+++ src/background.js
@@ -0,0 +1,5 @@
+browser.runtime.onMessage.addListener(
+  function (/*request, sender, sendResponse*/) {
+    console.log('Hello from the background')
+  }
+)
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pages: {
+    popup: {
+      template: 'public/browser-extension.html',
+      entry: './src/main.js',
+      // template: "public/browser-extension.html",
+      // entry: "./src/popup/main.js",
+      title: 'Popup',
+    },
+  },
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {
+        background: {
+          entry: 'src/background.js',
+        },
+      },
+    },
+  },
+}
